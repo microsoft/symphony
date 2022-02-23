5bba278f419b63a714dd553d7adcd8e82023319b
Hadwa Gaber <hadwaa@microsoft.com>
2022-02-22 23:27:04 -0800

    Update 02sql module terraform scripts

--- IAC/Terraform/terraform/02_sql/01_deployment/output.tf
+++ IAC/Terraform/terraform/02_sql/01_deployment/output.tf
@@ -1,9 +1,9 @@
 output "catalogdbcs" {
   sensitive = true
-  value = "Server=tcp:${azurerm_sql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.catalogdb.name};Persist Security Info=False;User ID=${random_string.sqlserverlogin.result};Password=${random_password.sqlserverpassword.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value     = "Server=tcp:${azurerm_sql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.catalogdb.name};Persist Security Info=False;User ID=${random_string.sqlserverlogin.result};Password=${random_password.sqlserverpassword.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 
 output "identitydbcs" {
   sensitive = true
-  value = "Server=tcp:${azurerm_sql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.identitydb.name};Persist Security Info=False;User ID=${random_string.sqlserverlogin.result};Password=${random_password.sqlserverpassword.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value     = "Server=tcp:${azurerm_sql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.identitydb.name};Persist Security Info=False;User ID=${random_string.sqlserverlogin.result};Password=${random_password.sqlserverpassword.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
--- IAC/Terraform/terraform/02_sql/01_deployment/provider.tf
+++ IAC/Terraform/terraform/02_sql/01_deployment/provider.tf
@@ -2,16 +2,23 @@
 # PROVIDERS
 ###########################################################################################################
 
-provider "azurerm" {
-  version = "~>2.85.0"
-  features {}
-}
-
-###########################################################################################################
-# BACKEND (REMOTE STATE)
-###########################################################################################################
-
+# Configure the Azure Provider
 terraform {
   required_version = "= v1.0.10"
   backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      version = "~>2.85.0"
+      source  = "hashicorp/azurerm"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "1.1.3"
+    }
+  }
 }
+
+provider "azurerm" {
+  features {}
+}
+
