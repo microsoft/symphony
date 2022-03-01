2f8a64eb5f74ca901009031e981edd3eeafc848d
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2022-03-01 14:33:58 -0800

    .

--- IAC/Bicep/deploy.sh
+++ IAC/Bicep/deploy.sh
@@ -14,13 +14,14 @@ resource_group_name=$(echo "${output}" | jq -r -c '.properties.outputs.name.valu
 
 # 01_sql | 02_deployment
 deployment_path="${layer_path}/02_deployment"
-sql_password=$(openssl rand -base64 14)
-output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters @parameters.${enviroment}.json "@${deployment_path}/parameters.${enviroment}.json" sqlServerAdministratorPassword="${sql_password}")
+# sql_password=$(openssl rand -base64 14)
+sql_password="demo!P@55w0rd123"
+output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters "@parameters.${enviroment}.json" "@${deployment_path}/parameters.${enviroment}.json" sqlServerAdministratorPassword="${sql_password}")
 catalogdb_cs=$(echo "${output}" | jq -r -c '.properties.outputs.sqlDatabaseCatalogDbCS.value')
 identitydb_cs=$(echo "${output}" | jq -r -c '.properties.outputs.sqlDatabaseIdentityDbCS.value')
 
 # 02_webapp
-layer_path="01_webapp"
+layer_path="02_webapp"
 
 # 02_webapp | 01_rg
 deployment_path="${layer_path}/01_rg"
@@ -29,4 +30,4 @@ resource_group_name=$(echo "${output}" | jq -r -c '.properties.outputs.name.valu
 
 # 02_webapp | 02_deployment
 deployment_path="${layer_path}/02_deployment"
-output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters @parameters.${enviroment}.json "@${deployment_path}/parameters.${enviroment}.json" catalogDbConnectionString="${catalogdb_cs} identityDbConnectionString="${identitydb_cs}")
+output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters "@parameters.${enviroment}.json" "@${deployment_path}/parameters.${enviroment}.json" catalogDbConnectionString="${catalogdb_cs}" identityDbConnectionString="${identitydb_cs}")
