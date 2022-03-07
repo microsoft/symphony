2926ad2447604b1e2b4285119a4bee19cd5c6c35
Engin Polat <enpolat@microsoft.com>
2022-03-07 14:59:45 -0800

    updating azurerm and azuread requirements to the latest versions

--- IAC/Terraform/terraform/01_init/provider.tf
+++ IAC/Terraform/terraform/01_init/provider.tf
@@ -15,4 +15,16 @@ provider "azuread" {
 
 terraform {
   required_version = ">= 1.1.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.98.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.18.0"
+    }
+  }
 }
