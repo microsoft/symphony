b50fa5da899dbc96a4539b013267695d7bc95b8a
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2022-03-07 22:21:28 -0800

    tfplan ext name

--- IAC/Terraform/terraform/deploy.sh
+++ IAC/Terraform/terraform/deploy.sh
@@ -6,8 +6,8 @@ declare enviroment="dev"
 layer_path="01_init"
 pushd "${layer_path}"
 terraform init
-terraform plan -var-file="${enviroment}.tfvars.json" -state="${enviroment}.tfstate" -out="${enviroment}.plan"
-terraform apply -state="${enviroment}.tfstate" -auto-approve "${enviroment}.plan"
+terraform plan -var-file="${enviroment}.tfvars.json" -state="${enviroment}.tfstate" -out="${enviroment}.tfplan"
+terraform apply -state="${enviroment}.tfstate" -auto-approve "${enviroment}.tfplan"
 
 tfstate_tfvars=$(cat "${enviroment}.tfvars.json")
 TFSTATE_RESOURCES_GROUP_NAME=$(echo ${tfstate_tfvars} | jq -r -c '.resource_group_name')
@@ -21,8 +21,8 @@ deployment_path="${layer_path}/01_deployment"
 TFSTATE_KEY="02_sql/01_deployment.tfstate"
 pushd "${deployment_path}"
 terraform init -migrate-state -backend-config=storage_account_name="${TFSTATE_STORAGE_ACCOUNT_NAME}" -backend-config=container_name="${TFSTATE_STORAGE_CONTAINER_NAME}" -backend-config=key="${TFSTATE_KEY}" -backend-config=resource_group_name="${TFSTATE_RESOURCES_GROUP_NAME}"
-terraform plan -var-file="${enviroment}.tfvars.json" -out="${enviroment}.plan"
-terraform apply -auto-approve "${enviroment}.plan"
+terraform plan -var-file="${enviroment}.tfvars.json" -out="${enviroment}.tfplan"
+terraform apply -auto-approve "${enviroment}.tfplan"
 popd
 
 # 02_webapp
