e9f08d1ca5753962fc72289780f0f269e2f196cf
Engin Polat <enpolat@microsoft.com>
2022-03-07 16:38:16 -0800

    formatting document

--- IAC/Terraform/terraform/02_sql/01_deployment/main.tf
+++ IAC/Terraform/terraform/02_sql/01_deployment/main.tf
@@ -1,20 +1,20 @@
 resource "random_string" "sqlserverlogin" {
-  length = 10
+  length  = 10
   special = false
-  number = false
+  number  = false
 }
 
 resource "random_password" "sqlserverpassword" {
-  length = 16
-  special = true
+  length           = 16
+  special          = true
   override_special = "#()-[]<>^*&#$"
 }
 
 module "naming" {
   source = "git::https://github.com/Azure/terraform-azurerm-naming?ref=0.1.0"
-  # levarage naming module.  Naming convention is resoure
-  prefix = [ var.ENVIRONMENT ]
-  suffix = [ var.NAME, "l02", "d01" ]
+  # levarage naming module. Naming convention is resoure
+  prefix = [var.ENVIRONMENT]
+  suffix = [var.NAME, "l02", "d01"]
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -44,7 +44,7 @@ resource "azurerm_sql_database" "catalogdb" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   server_name         = azurerm_sql_server.sqlserver.name
-  edition = "Basic"
+  edition             = "Basic"
 }
 
 resource "azurerm_sql_database" "identitydb" {
@@ -52,5 +52,5 @@ resource "azurerm_sql_database" "identitydb" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   server_name         = azurerm_sql_server.sqlserver.name
-  edition = "Basic"
+  edition             = "Basic"
 }
