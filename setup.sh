183ff3f318da318d3eeb5c5180d146154ec6a079
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2022-03-08 10:50:05 -0800

    removed ENV

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ENVIROMENT="dev"
 LOCATION="westus"
 APP_REPO="https://github.com/dotnet-architecture/eShopOnWeb.git"
 APP_COMMIT="a87f571"
@@ -12,10 +11,10 @@ APP_API_DOCKERFILE="src/PublicApi/Dockerfile"
 NAME=$(cat /dev/urandom | env LC_ALL=C tr -dc 'a-z' | fold -w 8 | head -n 1)
 NN=$(echo $RANDOM | fold -w 3 | head -n 1)
 
-RG_NAME="rg-${NAME}-${ENVIROMENT}-${NN}"
+RG_NAME="rg-${NAME}-${NN}"
 az group create --resource-group "${RG_NAME}" --location "${LOCATION}"
 
-CR_NAME="cr${NAME}${ENVIROMENT}${NN}"
+CR_NAME="cr${NAME}${NN}"
 az acr create --resource-group "${RG_NAME}" --location "${LOCATION}" --name "${CR_NAME}" --sku Basic
 
 git clone "${APP_REPO}" "_app"
