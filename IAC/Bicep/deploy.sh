93a88d405d43d913144d1ecbf259e030413609b1
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2022-03-11 03:00:50 +0000

    init

--- IAC/Bicep/deploy.sh
+++ IAC/Bicep/deploy.sh
@@ -5,28 +5,28 @@ parameters=$(cat parameters.${enviroment}.json)
 location=$(echo ${parameters} | jq -r -c '.parameters.location.value')
 
 # 01_sql
-layer_path="01_sql"
+layer_path="bicep/01_sql"
 
 # 01_sql | 01_rg
 deployment_path="${layer_path}/01_rg"
-output=$(az deployment sub create --template-file "${deployment_path}/main.bicep" --location "${location}" --parameters "@parameters.${enviroment}.json")
+output=$(az deployment sub create --template-file "${deployment_path}/main.bicep" --location "${location}" --parameters "@bicep/parameters.${enviroment}.json")
 resource_group_name=$(echo "${output}" | jq -r -c '.properties.outputs.name.value')
 
 # 01_sql | 02_deployment
 deployment_path="${layer_path}/02_deployment"
 sql_password=$(openssl rand -base64 14)
-output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters "@parameters.${enviroment}.json" "@${deployment_path}/parameters.${enviroment}.json" sqlServerAdministratorPassword="${sql_password}")
+output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters "@bicep/parameters.${enviroment}.json" "@${deployment_path}/parameters.${enviroment}.json" sqlServerAdministratorPassword="${sql_password}")
 catalogdb_cs=$(echo "${output}" | jq -r -c '.properties.outputs.sqlDatabaseCatalogDbCS.value')
 identitydb_cs=$(echo "${output}" | jq -r -c '.properties.outputs.sqlDatabaseIdentityDbCS.value')
 
 # 02_webapp
-layer_path="02_webapp"
+layer_path="bicep/02_webapp"
 
 # 02_webapp | 01_rg
 deployment_path="${layer_path}/01_rg"
-output=$(az deployment sub create --template-file "${deployment_path}/main.bicep" --location "${location}" --parameters "@parameters.${enviroment}.json")
+output=$(az deployment sub create --template-file "${deployment_path}/main.bicep" --location "${location}" --parameters "@bicep/parameters.${enviroment}.json")
 resource_group_name=$(echo "${output}" | jq -r -c '.properties.outputs.name.value')
 
 # 02_webapp | 02_deployment
 deployment_path="${layer_path}/02_deployment"
-output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters "@parameters.${enviroment}.json" "@${deployment_path}/parameters.${enviroment}.json" catalogDbConnectionString="${catalogdb_cs}" identityDbConnectionString="${identitydb_cs}")
+output=$(az deployment group create --resource-group "${resource_group_name}" --template-file "${deployment_path}/main.bicep" --parameters "@bicep/parameters.${enviroment}.json" "@${deployment_path}/parameters.${enviroment}.json" catalogDbConnectionString="${catalogdb_cs}" identityDbConnectionString="${identitydb_cs}")
---
+++ IAC/Bicep/test/resource_group.tests.ps1
@@ -0,0 +1,45 @@
+BeforeAll {
+    # your function
+    function Get-Planet ([string]$Name='*')
+    {
+        $planets = @(
+            @{ Name = 'Mercury' }
+            @{ Name = 'Venus'   }
+            @{ Name = 'Earth'   }
+            @{ Name = 'Mars'    }
+            @{ Name = 'Jupiter' }
+            @{ Name = 'Saturn'  }
+            @{ Name = 'Uranus'  }
+            @{ Name = 'Neptune' }
+        ) | foreach { [PSCustomObject]$_ }
+
+        $planets | where { $_.Name -like $Name }
+    }
+}
+
+# Pester tests
+Describe 'Get-Planet' {
+  It "Given no parameters, it lists all 8 planets" {
+    $allPlanets = Get-Planet
+    $allPlanets.Count | Should -Be 8
+  }
+
+  Context "Filtering by Name" {
+    It "Given valid -Name '<Filter>', it returns '<Expected>'" -TestCases @(
+      @{ Filter = 'Earth'; Expected = 'Earth' }
+      @{ Filter = 'ne*'  ; Expected = 'Neptune' }
+      @{ Filter = 'ur*'  ; Expected = 'Uranus' }
+      @{ Filter = 'm*'   ; Expected = 'Mercury', 'Mars' }
+    ) {
+      param ($Filter, $Expected)
+
+      $planets = Get-Planet -Name $Filter
+      $planets.Name | Should -Be $Expected
+    }
+
+    It "Given invalid parameter -Name 'Alpha Centauri', it returns `$null" {
+      $planets = Get-Planet -Name 'Alpha Centauri'
+      $planets | Should -Be $null
+    }
+  }
+}
