afb399b7c0a367bb11087ae64b174a7b95c939fe
polatengin <enpolat@microsoft.com>
2022-04-03 11:58:22 -0700

    adding terraform method and clear the previous instance leftovers

--- orchestrators/scripts/tests.runner.sh
+++ orchestrators/scripts/tests.runner.sh
@@ -14,3 +14,22 @@ print() {
   echo -e "--------------------------------------------------------------------------------\n[$(date)] : ${1}" | tee -a test.out
 }
 
+terraform() {
+  TEST_FILE_NAME=${1:-}
+
+  # cd into the terraform directory
+  cd ../../IAC/Terraform/
+
+  # cleanup any existing state
+  rm -rf ./terraform/**/.terraform
+  rm -rf ./terraform/**/*.tfplan
+  rm -rf ./terraform/**/*.tfstate
+  rm -rf ./terraform/backend.tfvars
+  rm -rf ./terraform/**/terraform.tfstate.backup
+
+  # cd to the test directory
+  cd ./test
+
+  CWD=$(pwd)
+
+}
