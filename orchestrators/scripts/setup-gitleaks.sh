a1762f91887202fe63a37d9e28bdca9fb6835be6
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2022-04-03 18:37:41 -0700

    pwsh

--- orchestrators/scripts/setup-gitleaks.sh
+++ orchestrators/scripts/setup-gitleaks.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Syntax: ./setup-gitleaks.sh [gitleaks version]
+# Syntax: ./setup-gitleaks.sh [version]
 
 # Includes
 source _helpers.sh
 source _setup_helpers.sh
 
 set -e
-GITLEAKS_VERSION="${1:-"latest"}"
+VERSION="${1:-"latest"}"
 
 # Get OS architecture
 get_os_architecture "x64" "arm64" "armv7" "x32"
 
 # Verify requested version is available, convert latest
-find_version_from_git_tags GITLEAKS_VERSION 'https://github.com/zricethezav/gitleaks'
+find_version_from_git_tags VERSION 'https://github.com/zricethezav/gitleaks'
 
 _information "Downloading Gitleaks..."
-gitleaks_filename="gitleaks_${GITLEAKS_VERSION}_linux_${os_architecture}.tar.gz"
-curl -sSL -o ${gitleaks_filename} "https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/${gitleaks_filename}"
-tar -xf ${gitleaks_filename} gitleaks
-rm -f ${gitleaks_filename}
+filename="gitleaks_${VERSION}_linux_${os_architecture}.tar.gz"
+curl -sSL -o "${filename}" "https://github.com/zricethezav/gitleaks/releases/download/v${VERSION}/${filename}"
+tar -xf "${filename}" gitleaks
+rm -f "${filename}"
 mv -f gitleaks /usr/local/bin/
---
+++ orchestrators/scripts/setup-powershell.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Syntax: ./setup-powershell.sh [version]
+
+# Includes
+source _helpers.sh
+source _setup_helpers.sh
+
+set -e
+VERSION="${1:-"latest"}"
+
+# Get OS architecture
+get_os_architecture "x64" "arm64" "arm32"
+
+# Verify requested version is available, convert latest
+find_version_from_git_tags VERSION 'https://github.com/PowerShell/PowerShell'
+
+_information "Downloading PowerShell..."
+filename="powershell-${VERSION}-linux-${os_architecture}.tar.gz"
+target_path="/opt/microsoft/powershell/$(echo ${VERSION} | grep -oE '[^\.]+' | head -n 1)"
+mkdir -p /tmp/pwsh "${target_path}"
+cd /tmp/pwsh
+curl -sSL -o "${filename}" "https://github.com/PowerShell/PowerShell/releases/download/v${VERSION}/${filename}"
+tar -xf "${filename}" -C "${target_path}"
+ln -s "${target_path}/pwsh" /usr/local/bin/pwsh
+rm -rf /tmp/pwsh
