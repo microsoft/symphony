920dad96ce1b88c1e370683adde6af13557d35cb
polatengin <enpolat@microsoft.com>
2022-04-13 22:53:12 -0700

    fixing tfvar file path

--- IAC/Terraform/terraform/deploy.sh
+++ IAC/Terraform/terraform/deploy.sh
@@ -5,10 +5,13 @@ declare environment="dev"
 # 01_init
 layer_path="01_init"
 pushd "${layer_path}"
+tfvar_file_path="../../../../env/terraform/${environment}/01_init.tfvars.json"
+
 terraform init
+terraform plan -var-file="${tfvar_file_path}" -state="${environment}.tfstate" -out="${environment}.tfplan"
 terraform apply -state="${environment}.tfstate" -auto-approve "${environment}.tfplan"
 
-tfstate_tfvars=$(cat "${enviroment}.tfvars.json")
+tfstate_tfvars=$(cat "${tfvar_file_path}")
 TFSTATE_RESOURCES_GROUP_NAME=$(echo ${tfstate_tfvars} | jq -r -c '.resource_group_name')
 TFSTATE_STORAGE_ACCOUNT_NAME=$(echo ${tfstate_tfvars} | jq -r -c '.storage_account_name')
 TFSTATE_STORAGE_CONTAINER_NAME=$(echo ${tfstate_tfvars} | jq -r -c '.container_name')
@@ -19,7 +22,9 @@ layer_path="02_sql"
 deployment_path="${layer_path}/01_deployment"
 TFSTATE_KEY="02_sql/01_deployment.tfstate"
 pushd "${deployment_path}"
+tfvar_file_path="../../../../env/terraform/${environment}/02_sql.tfvars.json"
 terraform init -migrate-state -backend-config=storage_account_name="${TFSTATE_STORAGE_ACCOUNT_NAME}" -backend-config=container_name="${TFSTATE_STORAGE_CONTAINER_NAME}" -backend-config=key="${TFSTATE_KEY}" -backend-config=resource_group_name="${TFSTATE_RESOURCES_GROUP_NAME}"
+terraform plan -var-file="${tfvar_file_path}" -out="${environment}.tfplan"
 terraform apply -auto-approve "${environment}.tfplan"
 popd
 
@@ -28,6 +33,8 @@ layer_path="03_webapp"
 deployment_path="${layer_path}/01_deployment"
 TFSTATE_KEY="03_webapp/01_deployment.tfstate"
 pushd "${deployment_path}"
+tfvar_file_path="../../../../env/terraform/${environment}/03_webapp.tfvars.json"
 terraform init -migrate-state -backend-config=storage_account_name="${TFSTATE_STORAGE_ACCOUNT_NAME}" -backend-config=container_name="${TFSTATE_STORAGE_CONTAINER_NAME}" -backend-config=key="${TFSTATE_KEY}" -backend-config=resource_group_name="${TFSTATE_RESOURCES_GROUP_NAME}"
+terraform plan -var-file="${tfvar_file_path}" -out="${environment}.plan"
 terraform apply -auto-approve "${environment}.plan"
 popd
