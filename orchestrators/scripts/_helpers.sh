77071c433ee003a14398786c8bfc566e70d94bb4
polatengin <enpolat@microsoft.com>
2022-04-18 16:19:43 -0700

    adding azlogin method

--- orchestrators/scripts/_helpers.sh
+++ orchestrators/scripts/_helpers.sh
@@ -31,3 +31,39 @@ _success() {
     _color="\e[32m" # green
     echo -e "${_color}##[command] $@\n\e[0m" 2>&1
 }
+
+azlogin() {
+    subscription_id=$1
+    tenant_id=$2
+    client_id=$3
+    client_secret=$4
+    cloud_name=$5
+
+    # AzureCloud AzureChinaCloud AzureUSGovernment AzureGermanCloud
+    az cloud set --name ${cloud_name}
+    az login --service-principal --username ${client_id} --password ${client_secret} --tenant ${tenant_id}
+    az account set --subscription ${subscription_id}
+
+    export ARM_CLIENT_ID="${client_id}"
+    export ARM_CLIENT_SECRET="${client_secret}"
+    export ARM_SUBSCRIPTION_ID="${subscription_id}"
+    export ARM_TENANT_ID="${tenant_id}"
+
+    # https://www.terraform.io/docs/providers/azurerm/index.html#environment
+    # environment - (Optional) The Cloud Environment which should be used. 
+    # Possible values are public, usgovernment, german, and china. Defaults to public. 
+    # This can also be sourced from the ARM_ENVIRONMENT environment variable.
+
+    if [ "${cloud_name}" == 'AzureCloud' ]; then
+        export ARM_ENVIRONMENT="public"
+    elif [ "${cloud_name}" == 'AzureUSGovernment' ]; then
+        export ARM_ENVIRONMENT="usgovernment"
+    elif [ "${cloud_name}" == 'AzureChinaCloud' ]; then 
+        export ARM_ENVIRONMENT="usgovernment" 
+    elif [ "${cloud_name}" == 'AzureGermanCloud' ]; then
+        export ARM_ENVIRONMENT="german" 
+    else
+        _error "Unknown cloud. Check documentation https://www.terraform.io/docs/providers/azurerm/index.html#environment"
+        return 1
+    fi
+}
