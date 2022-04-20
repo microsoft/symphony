d8bebb188e5f66fc3b044fef60c9e08c4a7e754d
Hadwa Gaber <hadwaa@microsoft.com>
2022-04-20 13:12:18 -0700

    fix tflint typo

--- orchestrators/scripts/setup-tflint.sh
+++ orchestrators/scripts/setup-tflint.sh
@@ -15,9 +15,9 @@ get_os_architecture
 # Verify requested version is available, convert latest
 find_version_from_git_tags VERSION 'https://github.com/terraform-linters/tflint'
 
-_information "Downloading Tetflint..."
+_information "Downloading Tflint..."
 filename="tflint_linux_${os_architecture}.zip"
-curl -sSL -o "${filename}" "htttps://github.com/terraform-linters/tflint/releases/download/${VERSION}/${filename}"
+curl -sSL -o "${filename}" "https://github.com/terraform-linters/tflint/releases/download/${VERSION}/${filename}"
 unzip "${filename}"
 rm -f "${filename}"
 mv -f tflint /usr/local/bin/
