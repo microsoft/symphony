fc6b5518cc5c229f21f2ff0f51dc1b92c6c7856c
Hadwa Gaber <hadwaa@microsoft.com>
2022-04-20 12:49:12 -0700

    add setup tflint script

---
+++ orchestrators/scripts/setup-tflint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Syntax: ./setup-terraform.sh [version]
+
+# Includes
+source _helpers.sh
+source _setup_helpers.sh
+
+set -e
+VERSION="${1:-"latest"}"
+
+# Get OS architecture
+get_os_architecture
+
+# Verify requested version is available, convert latest
+find_version_from_git_tags VERSION 'https://github.com/terraform-linters/tflint'
+
+_information "Downloading Tetflint..."
+filename="terraform_${VERSION}_linux_${os_architecture}.zip"
+curl -sSL -o "${filename}" "htttps://github.com/terraform-linters/tflint/${VERSION}/${filename}"
+unzip "${filename}"
+rm -f "${filename}"
+mv -f tflint /usr/local/bin/
