f0f77ee3b871ce7071b7dd06b9fa6aae9acff71e
Hadwa Gaber <hadwaa@microsoft.com>
2022-04-20 13:20:43 -0700

    add v in download links

--- orchestrators/scripts/setup-tflint.sh
+++ orchestrators/scripts/setup-tflint.sh
@@ -15,9 +15,9 @@ get_os_architecture
 # Verify requested version is available, convert latest
 find_version_from_git_tags VERSION 'https://github.com/terraform-linters/tflint'
 
-_information "Downloading Tflint..."
+_information "Downloading Tflint v${VERSION} ..."
 filename="tflint_linux_${os_architecture}.zip"
-curl -sSL -o "${filename}" "https://github.com/terraform-linters/tflint/releases/download/${VERSION}/${filename}"
+curl -sSL -o "${filename}" "https://github.com/terraform-linters/tflint/releases/download/v${VERSION}/${filename}"
 unzip "${filename}"
 rm -f "${filename}"
 mv -f tflint /usr/local/bin/
