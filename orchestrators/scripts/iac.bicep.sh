e07aff41c69aaaf644ccc638e8501d26ab8d5211
Engin Polat <enpolat@microsoft.com>
2022-05-04 22:57:48 -0700

    script updates (#41)

--- orchestrators/scripts/iac.bicep.sh
+++ orchestrators/scripts/iac.bicep.sh
@@ -19,26 +19,28 @@ lint() {
 }
 
 validate() {
-    scope=$1
-    bicep_file_path=$2
-    bicep_parameters_file_path=$3
-    deployment_id=$4
-    location=$5
-    optional_parameters=$6 # --management-group-id or --resource-group
+    bicep_file_path=$1
+    bicep_parameters_file_path=$2
+    deployment_id=$3
+    location=$4
+    optional_parameters=$5 # --management-group-id or --resource-group
 
     _information "Execute Bicep validate"
 
-    if [[ "${scope}" == "mg" ]]; then
+    targetScope=$(grep -oP 'targetScope\s*=\s*\K[^\s]+' ${bicep_file_path} | sed -e 's/[\"\`]//g')
+    targetScope=${targetScope//\'/}
+
+    if [[ "${targetScope}" == "mg" ]]; then
         az deployment mg validate --management-group-id "${optional_parameters}" --location "${location}" --name "${deployment_id}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
-    elif [[ "${scope}" == "sub" ]]; then
+    elif [[ "${targetScope}" == "sub" ]]; then
         az deployment sub validate --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
-    elif [[ "${scope}" == "tenant" ]]; then
+    elif [[ "${targetScope}" == "tenant" ]]; then
         az deployment tenant validate --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
     else
         az deployment group validate --resource-group "${optional_parameters}" --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
     fi
 
-    exit $?
+    return $?
 }
 
 preview() {
