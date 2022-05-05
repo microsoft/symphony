1b4e418cff5f21a673d04990ea6436d1e84df9fa
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2022-05-05 17:00:14 +0200

    .

--- orchestrators/scripts/iac.bicep.sh
+++ orchestrators/scripts/iac.bicep.sh
@@ -39,25 +39,24 @@ validate() {
     targetScope=$(_target_scope "${bicep_file_path}")
 
     if [[ "${targetScope}" == "managementGroup" ]]; then
-        az deployment mg validate --management-group-id "${optional_parameters}" --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
+        az deployment mg validate --management-group-id "${optional_parameters}" --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "${bicep_parameters_file_path}"
     elif [[ "${targetScope}" == "subscription" ]]; then
-        az deployment sub validate --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
+        az deployment sub validate --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "${bicep_parameters_file_path}"
     elif [[ "${targetScope}" == "tenant" ]]; then
-        az deployment tenant validate --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
+        az deployment tenant validate --name "${deployment_id}" --location "${location}" --template-file "${bicep_file_path}" --parameters "${bicep_parameters_file_path}"
     else
-        az deployment group validate --resource-group "${optional_parameters}" --name "${deployment_id}" --template-file "${bicep_file_path}" --parameters "@${bicep_parameters_file_path}"
+        az deployment group validate --resource-group "${optional_parameters}" --name "${deployment_id}" --template-file "${bicep_file_path}" --parameters "${bicep_parameters_file_path}"
     fi
 
     return $?
 }
 
 preview() {
-    scope=$1
-    bicep_file_path=$2
-    bicep_parameters_file_path=$3
-    deployment_id=$4
-    location=$5
-    optional_parameters=$6 # --management-group-id or --resource-group
+    bicep_file_path=$1
+    bicep_parameters_file_path=$2
+    deployment_id=$3
+    location=$4
+    optional_parameters=$5 # --management-group-id or --resource-group
 
     _information "Execute Bicep preview"
 
