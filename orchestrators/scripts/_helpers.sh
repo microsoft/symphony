86ee9c69f497f3bc5804ead0c06dc24c5f257a54
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2022-06-06 12:26:02 -0700

    .

--- orchestrators/scripts/_helpers.sh
+++ orchestrators/scripts/_helpers.sh
@@ -8,28 +8,33 @@ _debug() {
     # Only print debug lines if debugging is turned on.
     if [ ${DEBUG_FLAG} == true ]; then
         _color="\e[35m" # magenta
-        echo -e "${_color}##[debug] $@\n\e[0m" 2>&1
+        # echo -e "${_color}##[debug] $@\n\e[0m" 2>&1
+        echo -e "${_color}::debug::$@\n\e[0m" 2>&1
     fi
 }
 
 _error() {
     _color="\e[31m" # red
-    echo -e "${_color}##[error] $@\n\e[0m" 2>&1
+    # echo -e "${_color}##[error] $@\n\e[0m" 2>&1
+    echo -e "${_color}::error::$@\n\e[0m" 2>&1
 }
 
 _warning() {
     _color="\e[33m" # yellow
-    echo -e "${_color}##[warning] $@\n\e[0m" 2>&1
+    # echo -e "${_color}##[warning] $@\n\e[0m" 2>&1
+    echo -e "${_color}::warning::$@\n\e[0m" 2>&1
 }
 
 _information() {
     _color="\e[36m" # cyan
-    echo -e "${_color}##[command] $@\n\e[0m" 2>&1
+    # echo -e "${_color}##[command] $@\n\e[0m" 2>&1
+    echo -e "${_color}::notice::$@\n\e[0m" 2>&1
 }
 
 _success() {
     _color="\e[32m" # green
-    echo -e "${_color}##[command] $@\n\e[0m" 2>&1
+    # echo -e "${_color}##[command] $@\n\e[0m" 2>&1
+    echo -e "${_color}::notice::$@\n\e[0m" 2>&1
 }
 
 azlogin() {
@@ -71,13 +76,16 @@ azlogin() {
 parse_bicep_parameters() {
     local bicep_parameters_file_path=$1
 
-    _information "Parsing parameter file with Envs: ${bicep_parameters_file_path}"
-
-    fileContentCheck=$(cat "${bicep_parameters_file_path}" | jq '.parameters' | grep "\\$")
-    if [ ! -z "${fileContentCheck}" ]; then
-        echo $(cat "${bicep_parameters_file_path}") | jq '.parameters |= map_values(if .value | (startswith("$") and env[.[1:]]) then .value |= env[.[1:]] else . end)' >"${bicep_parameters_file_path}"
+    if [[ -f "${bicep_parameters_file_path}" ]]; then
+        _information "Parsing parameter file with Envs: ${bicep_parameters_file_path}"
+        fileContentCheck=$(cat "${bicep_parameters_file_path}" | jq '.parameters' | grep "\\$")
+        if [ ! -z "${fileContentCheck}" ]; then
+            echo $(cat "${bicep_parameters_file_path}") | jq '.parameters |= map_values(if .value | (startswith("$") and env[.[1:]]) then .value |= env[.[1:]] else . end)' >"${bicep_parameters_file_path}"
+        fi
     fi
 }
+
+parse_bicep_parameters "/mnt/c/gh/symphony-1/env/bicep/dev/parameters.json"
 
 bicep_output_to_env() {
     local bicep_output_json=$1
