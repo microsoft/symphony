2144da32432157a586f4bfebd93396f180fcbbb1
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2022-06-08 16:28:36 -0700

    .

--- orchestrators/scripts/iac.bicep.sh
+++ orchestrators/scripts/iac.bicep.sh
@@ -109,6 +109,7 @@ validate() {
 
     return $exit_code
 }
+export -f validate
 
 preview() {
     local bicep_file_path=$1
--- orchestrators/scripts/iac.bicep.validate.gh.sh
+++ orchestrators/scripts/iac.bicep.validate.gh.sh
@@ -35,7 +35,7 @@ for deployment in "${modules[@]}"; do
         fi
     done
 
-    output=$(validate "${deployment}" params_path "${GITHUB_RUN_ID}" "${LOCATION}" "rg-validate")
+    output=$(validate "${deployment}" params_path "${GITHUB_RUN_ID}" "${LOCATION}" 'rg-validate')
     exit_code=$?
 
     if [[ $exit_code != 0 ]]; then
