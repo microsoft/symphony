9b551b17c65bc71e99157cd407804c1274112d10
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2022-06-08 11:56:25 -0700

    .

---
+++ IAC/Bicep/scripts/pre.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sqlServerAdministratorLogin="testadmin"
+sqlServerAdministratorPassword="Test#@rdP@ssw0rd123!"
+
+echo "sqlServerAdministratorLogin=${sqlServerAdministratorLogin}" >> $GITHUB_ENV
+echo "sqlServerAdministratorPassword=${sqlServerAdministratorPassword}" >> $GITHUB_ENV
