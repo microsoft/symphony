35fa10687eec45b2c457fc0abfa8c1ac4869cd7a
Engin Polat <enpolat@microsoft.com>
2022-06-16 00:50:20 +0000

    adding deploy step

---
+++ orchestrators/scripts/iac.bicep.deploy.azdo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+pushd .
+
+cd "${WORKSPACE_PATH}/IAC/Bicep/bicep"
+
+SAVEIFS=$IFS
+IFS=$'\n'
+modules=($(find . -type f -name 'main.bicep' | sort -u))
+IFS=$SAVEIFS
+
+for deployment in "${modules[@]}"; do
+    _information "Executing Bicep deploy: ${deployment}"
+
+    path=$(dirname "${deployment}")
+
+    params=()
+    SAVEIFS=$IFS
+    IFS=$'\n'
+    params=($(find "${WORKSPACE_PATH}/env/bicep/${ENVIRONMENT}" -maxdepth 1 -type f -name '*parameters*.json'))
+    param_tmp_deployment="${WORKSPACE_PATH}/env/bicep/${ENVIRONMENT}/${path//.\//}/"
+    if [[ -d "${param_tmp_deployment}" ]]; then
+        params+=($(find "${param_tmp_deployment}" -maxdepth 1 -type f -name '*parameters*.json' -and -not -name '*mockup*'))
+    fi
+    IFS=$SAVEIFS
+
+    params_path=()
+    for param_path_tmp in "${params[@]}"; do
+        if [[ -f "${param_path_tmp}" ]]; then
+            parse_bicep_parameters "${param_path_tmp}"
+            params_path+=("${param_path_tmp}")
+        fi
+    done
+
+    output=$(deploy "${deployment}" params_path "${RUN_ID}" "${LOCATION}" "${resource_group_name}")
+    exit_code=$?
+
+    if [[ $exit_code != 0 ]]; then
+        _error "Bicep deploy failed - returned code ${exit_code}"
+        exit $exit_code
+    fi
+
+    bicep_output_to_env "${output}"
+
+    echo "------------------------"
+done
+
+popd
