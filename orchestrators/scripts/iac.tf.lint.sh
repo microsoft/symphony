b65bea488a901d44000a6db10dc4be585578c61d
Hadwa Gaber <hadwaa@microsoft.com>
2022-06-24 14:43:47 -0700

    add tflint

---
+++ orchestrators/scripts/iac.tf.lint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 'Run tflint'             
+source ./iac.tf.sh
+pushd .
+cd ./../../IAC/Terraform/terraform
+modules=$(find . -type d | sort | awk '$0 !~ last "/" {print last} {last=$0} END {print last}')
+
+SAVEIFS=$IFS
+IFS=$'\n'
+array=($modules)
+IFS=$SAVEIFS
+len=${#array[@]}
+for deployment in "${array[@]}"
+    do
+    _information "Executing tf lint for: ${deployment}"
+    pushd .
+    cd $deployment
+    lint
+    code=$?
+    if [[ $code != 0 ]]; then
+        _error "tflint failed- returned code ${code}" 
+        exit $code
+    fi
+    popd
+
+    done
+popd
