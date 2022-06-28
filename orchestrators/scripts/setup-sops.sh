675520ed9f1eb6a01e1c44421aa446b26d064593
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2022-06-28 08:47:25 -0700

    setup sops

---
+++ orchestrators/scripts/setup-sops.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Syntax: ./setup-sops.sh [version]
+
+# Includes
+source _helpers.sh
+source _setup_helpers.sh
+
+set -e
+VERSION="${1:-"latest"}"
+
+# Get OS architecture
+get_os_architecture "amd64" "arm64"
+
+# Verify requested version is available, convert latest
+find_version_from_git_tags VERSION 'https://github.com/mozilla/sops'
+
+_information "Downloading SOPS..."
+
+# https://github.com/mozilla/sops/releases/download/v3.7.3/sops-v3.7.3.linux.amd64
+
+filename="sops-v${VERSION}.linux.${os_architecture}"
+echo "${filename}"
+curl -sSL -o "${filename}" "https://github.com/mozilla/sops/releases/download/v${VERSION}/${filename}"
+mv -f "${filename}" /usr/local/bin/sops
