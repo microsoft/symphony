b1be1d287bb10b9c190b34181702b8c5b9b6c785
Engin Polat <enpolat@microsoft.com>
2022-07-07 23:51:21 +0000

    adding azure devops support to helpers

--- orchestrators/scripts/_helpers.sh
+++ orchestrators/scripts/_helpers.sh
@@ -9,12 +9,10 @@ _debug_json() {
 _debug() {
     # Only print debug lines if debugging is turned on.
     if [ ${DEBUG_FLAG} == true ]; then
-        _color="\e[35m" # magenta
-        # echo -e "${_color}##[debug] $@\n\e[0m" 2>&1
-        # echo -e "${_color}::debug::$@\n\e[0m" 2>&1
-
         if [ -n "${GITHUB_ACTION}" ]; then
             echo "::debug::$@"
+        elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+            echo -e "##[debug]$@"
         else
             echo "DEBUG: $@"
         fi
@@ -22,48 +20,40 @@ _debug() {
 }
 
 _error() {
-    _color="\e[31m" # red
-    # echo -e "${_color}##[error] $@\n\e[0m" 2>&1
-    # echo -e "${_color}::error::$@\n\e[0m" 2>&1
-
     if [ -n "${GITHUB_ACTION}" ]; then
         echo "::error::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[error]$@"
     else
         echo "ERROR: $@"
     fi
 }
 
 _warning() {
-    _color="\e[33m" # yellow
-    # echo -e "${_color}##[warning] $@\n\e[0m" 2>&1
-    # echo -e "${_color}::warning::$@\n\e[0m" 2>&1
-
     if [ -n "${GITHUB_ACTION}" ]; then
         echo "::warning::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[warning]$@"
     else
         echo "WARNING: $@"
     fi
 }
 
 _information() {
-    _color="\e[36m" # cyan
-    # $AGENT_NAME ADO?
-    # echo -e "${_color}##[command] $@\n\e[0m" 2>&1
-
     if [ -n "${GITHUB_ACTION}" ]; then
         echo "::notice::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[command]$@"
     else
         echo "NOTICE: $@"
     fi
 }
 
 _success() {
-    _color="\e[32m" # green
-    # echo -e "${_color}##[command] $@\n\e[0m" 2>&1
-    # echo -e "${_color}::notice::$@\n\e[0m" 2>&1
-
     if [ -n "${GITHUB_ACTION}" ]; then
         echo "::notice::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[section]$@"
     else
         echo "NOTICE: $@"
     fi
