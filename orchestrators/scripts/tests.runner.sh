fae41e2bd5f291d8c175f65dbc6a3d26096a661d
Engin Polat <enpolat@microsoft.com>
2022-07-11 21:40:17 +0000

    adding az cli login at the beginning of e2e tests

--- orchestrators/scripts/tests.runner.sh
+++ orchestrators/scripts/tests.runner.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source ./iac.bicep.sh
+azlogin "${ARM_SUBSCRIPTION_ID}" "${ARM_TENANT_ID}" "${ARM_CLIENT_ID}" "${ARM_CLIENT_SECRET}" 'AzureCloud'
+
 # include helpers
 source _helpers.sh
 
