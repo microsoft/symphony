07d9f6fd86fc30c9fe81743db55b0f10cf75e059
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2022-07-13 19:18:01 -0700

    tests

--- IAC/Bicep/bicep/01_sql/02_deployment/main.bicep
+++ IAC/Bicep/bicep/01_sql/02_deployment/main.bicep
@@ -60,6 +60,8 @@ module sqlDatabaseIdentityDb './modules/sqlDatabase.bicep' = {
   ]
 }
 
+output sqlServerResourceGroupName string = sqlServerName.outputs.name
+output sqlServerName string = sqlServerName.outputs.name
 output sqlServerFqdn string = sqlServer.outputs.fqdn
 output sqlDatabaseCatalogDbName string = sqlDatabaseCatalogDb.outputs.name
 output sqlDatabaseIdentityDbName string = sqlDatabaseIdentityDb.outputs.name
--- IAC/Bicep/test/spec/sql_integration_spec.sh
+++ IAC/Bicep/test/spec/sql_integration_spec.sh
@@ -1,25 +1,25 @@
 Describe 'SQL Integration Tests'
     setup(){
-      export RG_NAME="dev-rg-sql-zy4"
-      export SQL_SERVER_NAME="dev-sqlserver-tl3"
+      export SQL_SERVER_RG_NAME="${sqlServerResourceGroupName}"
+      export SQL_SERVER_NAME="${sqlServerName}"
     }
     BeforeEach 'setup'
 
     Context 'SQL Server'
       It "SQL Server should has state 'Ready'"
-        When call get_sql_server_by_name "${RG_NAME}" "${SQL_SERVER_NAME}"
+        When call get_sql_server_by_name "${SQL_SERVER_RG_NAME}" "${SQL_SERVER_NAME}"
         The output should include_json '.state' 'Ready'
       End
     End
 
     Context 'SQL Databases'
       It "CatalogDB should has status 'Online'"
-        When call get_sql_database_by_name "${RG_NAME}" "${SQL_SERVER_NAME}" "catalogdb"
+        When call get_sql_database_by_name "${SQL_SERVER_RG_NAME}" "${SQL_SERVER_NAME}" "catalogdb"
         The output should include_json '.status' 'Online'
       End
 
       It "IdentityDB should has status 'Online'"
-        When call get_sql_database_by_name "${RG_NAME}" "${SQL_SERVER_NAME}" "identitydb"
+        When call get_sql_database_by_name "${SQL_SERVER_RG_NAME}" "${SQL_SERVER_NAME}" "identitydb"
         The output should include_json '.status' 'Online'
       End
     End
