aae6d96cf047a4c546d58fad9904e51aae57fb56
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2022-07-16 11:20:39 -0700

    .

--- orchestrators/scripts/iac.tf.validate.sh
+++ orchestrators/scripts/iac.tf.validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source ./iac.tf.sh
-pushd ${WORKSPACE_PATH}/IAC/Terraform/terraform
+pushd "${WORKSPACE_PATH}/IAC/Terraform/terraform"
 modules=$(find . -type d | sort | awk '$0 !~ last "/" {print last} {last=$0} END {print last}')
 
 SAVEIFS=$IFS
@@ -9,7 +9,7 @@ IFS=$'\n'
 array=($modules)
 IFS=$SAVEIFS
 len=${#array[@]}
-echo "Az login"
+
 azlogin "${ARM_SUBSCRIPTION_ID}" "${ARM_TENANT_ID}" "${ARM_CLIENT_ID}" "${ARM_CLIENT_SECRET}" 'AzureCloud'
 for deployment in "${array[@]}"; do
     if [[ ${deployment} != *"01_init"* ]]; then
