b4003d9bd040a1fbcc518525b07f6c927fa2f804
Engin Polat <enpolat@microsoft.com>
2022-08-12 04:17:07 +0000

    moving tests into one end-to-end test file

--- IAC/Bicep/test/end_to_end/End_To_End.Tests.ps1
+++ IAC/Bicep/test/end_to_end/End_To_End.Tests.ps1
@@ -3,20 +3,47 @@ BeforeDiscovery {
 }
 
 BeforeAll {
-    $sqlServerResourceGroupName = "rgsqllayer"
-    $sqlServerName = "test-sqlserver-pyo"
-    $resourceGroupName = "dev-rg-web-nnl"
-    $appServicePlanName = "dev-app-svc-plan-k55"
-    $appServiceName = "dev-app-svc-guw"
+    $sqlServerResourceGroupName = $env:SQLSERVER_RESOURCE_GROUP_NAME
+    $sqlServerName = $env:SQLSERVER_NAME
+    $resourceGroupName = $env:APPSERVICE_RESOURCE_GROUP_NAME
+    $appServicePlanName = $env:APPSERVICE_PLAN_NAME
+    $appServiceName = $env:APPSERVICE_NAME
 }
 
 Describe "End to End Tests" {
     Context "Test end to end" {
-        It "resources should be ready and online, and web app works" -TestCases @(
-            @{ expected = 'Ready' }
-        ) {
+        It "should check the parameters and environment variables configured correctly" {
+          $sqlServerResourceGroupName | Should -Not -Be $Null
+          $sqlServerName | Should -Not -Be $Null
+        }
+
+        It "should check the azure powershell connected and configured correctly" {
+          $currentContext = Get-AzContext
+          $currentContext | Should -Not -Be $Null
+        }
+
+        It "resources should be ready and online, and web app works" {
             $sqlServerResource = Get-SqlServer $sqlServerName $sqlServerResourceGroupName
-            $sqlServerResource.State | Should -Be ""
+            $sqlServerResource.PublicNetworkAccess | Should -Be "Enabled"
+
+            $identitydbResource = Get-SqlDatabase "identitydb" $sqlServerName $sqlServerResourceGroupName
+            $identitydbResource.Status | Should -Be "Online"
+
+            $catalogdbResource = Get-SqlDatabase "catalogdb" $sqlServerName $sqlServerResourceGroupName
+            $catalogdbResource.Status | Should -Be "Online"
+
+            $appServicePlanResource = Get-AppServicePlan $appServicePlanName $resourceGroupName
+            $appServicePlanResource.Status | Should -Be "Ready"
+            
+            $webAppResource = Get-WebApp $appServiceName $resourceGroupName
+            $webAppResource.State | Should -Be "Running"
+
+            $defaultHostName = $webAppResource.DefaultHostName
+            $defaultHostName | Should -Not -Be $Null
+
+            $response = Invoke-RestMethod -Uri "http://$defaultHostName" -Method Get;
+
+            Write-Host ($response | ConvertTo-Json)
         }
     }
 }
