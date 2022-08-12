8aa010796265d7a25f178b215383808057cfcf62
Engin Polat <enpolat@microsoft.com>
2022-08-12 04:46:34 +0000

    formatting document

--- IAC/Bicep/test/utils/AzCli.psm1
+++ IAC/Bicep/test/utils/AzCli.psm1
@@ -1,8 +1,5 @@
 function Invoke-AzCli($Command) {
     $toExecute = "az $Command"
-    # Write-Host "Executing:"
-    # Write-Host "$toExecute"
-    # Write-Host
     $result = Invoke-Expression "$toExecute"
     if ($LastExitCode -gt 0) {
         Write-Error $result
