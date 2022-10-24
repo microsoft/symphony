3c022414aaba9c8bd61803e955dde9ed05d4c276
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2022-10-24 12:09:23 -0700

    added azdo and github providers for bootstrapper

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source scripts/utilities/shell_logger.sh
 source scripts/install/banner.sh
 
-show_banner
+_information "Symphony Install"
+
---
+++ scripts/utilities/shell_logger.sh
@@ -0,0 +1,29 @@
+# Logger Functions with colors for Bash Shell
+
+_error() {
+    printf "\e[31mERROR: $@\n\e[0m"
+}
+
+_debug() {
+    #Only print debug lines if debugging is turned on.
+    if [ "$DEBUG_FLAG" == true ]; then
+        msg="$@"
+        LIGHT_CYAN='\033[0;35m'
+        NC='\033[0m'
+        printf "DEBUG: ${NC} %s ${NC}\n" "${msg}"
+    fi
+}
+
+_debug_json() {
+    if [ "$DEBUG_FLAG" == true ]; then
+        echo $1 | jq
+    fi
+}
+
+_information() {
+    printf "\e[36m$@\n\e[0m"
+}
+
+_success() {
+    printf "\e[32m$@\n\e[0m"
+}
