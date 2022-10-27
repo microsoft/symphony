b46593117e7173eac10b76b702755b5640ea8480
Hadwa Gaber <hadwaa@microsoft.com>
2022-10-27 12:49:55 -0700

    update azdo script

--- scripts/utilities/shell_logger.sh
+++ scripts/utilities/shell_logger.sh
@@ -1,29 +1,54 @@
 # Logger Functions with colors for Bash Shell
 
-_error() {
-    printf "\e[31mERROR: $@\n\e[0m"
-}
-
 _debug() {
-    #Only print debug lines if debugging is turned on.
-    if [ "$DEBUG_FLAG" == true ]; then
-        msg="$@"
-        LIGHT_CYAN='\033[0;35m'
-        NC='\033[0m'
-        printf "DEBUG: ${NC} %s ${NC}\n" "${msg}"
+    # Only print debug lines if debugging is turned on.
+    if [ -n ${DEBUG_FLAG} ]; then
+        if [ -n "${GITHUB_ACTION}" ]; then
+            echo "::debug::$@"
+        elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+            echo -e "##[debug]$@"
+        else
+            echo "DEBUG: $@"
+        fi
     fi
 }
 
-_debug_json() {
-    if [ "$DEBUG_FLAG" == true ]; then
-        echo $1 | jq
+_error() {
+    if [ -n "${GITHUB_ACTION}" ]; then
+        echo "::error::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[error]$@"
+    else
+        echo "ERROR: $@"
+    fi
+}
+
+_warning() {
+    if [ -n "${GITHUB_ACTION}" ]; then
+        echo "::warning::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[warning]$@"
+    else
+        echo "WARNING: $@"
     fi
 }
 
 _information() {
-    printf "\e[36m$@\n\e[0m"
+    if [ -n "${GITHUB_ACTION}" ]; then
+        echo "::notice::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[command]$@"
+    else
+        echo "NOTICE: $@"
+    fi
 }
 
 _success() {
-    printf "\e[32m$@\n\e[0m"
+    if [ -n "${GITHUB_ACTION}" ]; then
+        echo "::notice::$@"
+    elif [ -n "${SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}" ]; then
+        echo -e "##[section]$@"
+    else
+        echo "NOTICE: $@"
+    fi
 }
