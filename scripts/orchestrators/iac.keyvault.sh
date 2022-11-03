8c1421a1c2532efb20389989e17ee3c586caac41
Hadwa Gaber <hadwaa@microsoft.com>
2022-11-03 16:54:52 -0700

    .
    Co-authored-by: Hattan Shobokshi <hattan.shobokshi@microsoft.com>

--- scripts/orchestrators/iac.keyvault.sh
+++ scripts/orchestrators/iac.keyvault.sh
@@ -6,6 +6,6 @@ for secretname in "${SECRETS}"
 do
 secret=$(az keyvault secret show --name $secretname --vault-name "${KEYVAULT_NAME}" --query "value")
 echo "::add-mask::$secret"        
-echo "$secretname=$secret" >> $GITHUB_OUTPUT
+echo "$secretname=$secret" >> $G_OUTPUT
 done
 IFS=$OIFS
