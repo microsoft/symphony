6bd09a9e2d237436abda44448a8d7c2a17c02b9e
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2022-11-09 09:05:52 -0800

    Removing Bicep pipeline debug statements and fixing upload artifact path
    Co-authored-by: Hadwa Gaber <hadwaa@microsoft.com>

--- scripts/orchestrators/iac.bicep.previewdeploy.sh
+++ scripts/orchestrators/iac.bicep.previewdeploy.sh
@@ -81,8 +81,6 @@ for deployment in "${modules[@]}"; do
     fi
 
     bicep_output_to_env "${output}" ".env" "false" "true"
-    echo " ------------ DOT ENV DIR   $(pwd) ------------"
-    cat .env
 
     echo "${output}"
     echo "------------------------"
