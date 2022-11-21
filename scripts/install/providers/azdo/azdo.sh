e76566bb3f0d9be7692f1278541a8ba76f45938c
Hadwa Gaber <hadwaa@microsoft.com>
2022-11-21 09:50:32 -0800

    Update Terraform AzDo ci  (#89)

--- scripts/install/providers/azdo/azdo.sh
+++ scripts/install/providers/azdo/azdo.sh
@@ -136,6 +136,8 @@ function create_pipelines_terraform() {
     pipelineVariables="$pipelineVariables, $(_get_pipeline_var_defintion keyVaultName kv-symphony-environments true)"
     pipelineVariables="$pipelineVariables, $(_get_pipeline_var_defintion goVersion 1.18.1 true)" 
     pipelineVariables="$pipelineVariables, $(_get_pipeline_var_defintion terraformVersion 1.1.7 true)" 
+    pipelineVariables="$pipelineVariables, $(_get_pipeline_var_defintion runLayerTest false true)"
+    pipelineVariables="$pipelineVariables, $(_get_pipeline_var_defintion runBackupState true true)"   
     _create_pipeline "CI-Deploy" "/.azure-pipelines/pipeline.ci.terraform.yml" "Deploy" "${pipelineVariables}" "${AZDO_PROJECT_NAME}"
 
 
