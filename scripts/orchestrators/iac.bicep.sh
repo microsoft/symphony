5e9c408c41e6f60dc8794274c0fcd4a03bce2a9e
Engin Polat <enpolat@microsoft.com>
2023-02-03 22:07:02 +0000

    adding pre_deploy and post_deploy functions support

--- scripts/orchestrators/iac.bicep.sh
+++ scripts/orchestrators/iac.bicep.sh
@@ -174,9 +174,18 @@ deploy() {
     local deployment_id=$3
     local location=$4
     local optional_args=$5 # --management-group-id or --resource-group
+    local layer_folder_path=$(dirname "${bicep_file_path}")
 
     target_scope=$(_target_scope "${bicep_file_path}")
     bicep_parameters=$(_bicep_parameters bicep_parameters_file_path_array)
+
+    if [ -f "${layer_folder_path}/_events.sh" ]; then
+      source "${layer_folder_path}/_events.sh"
+
+      if [ "$(type -t pre_deploy)" == "function" ]; then
+        pre_deploy
+      fi
+    fi
 
     if [[ "${target_scope}" == "managementGroup" ]]; then
         command="az deployment mg create --management-group-id ${optional_args} --name ${deployment_id} --location ${LOCATION_NAME} --template-file ${bicep_file_path} ${bicep_parameters}"
@@ -190,6 +199,19 @@ deploy() {
 
     output=$(eval "${command}")
     exit_code=$?
+
+    if [ ${exit_code} -eq 0 ]; then
+      if [ -f "${layer_folder_path}/_events.sh" ]; then
+        source "${layer_folder_path}/_events.sh"
+
+        if [ "$(type -t post_deploy)" == "function" ]; then
+          post_deploy
+        fi
+      fi
+    fi
+
+    unset -f pre_deploy
+    unset -f post_deploy
 
     echo "${output}"
 
