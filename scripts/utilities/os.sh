e55032da4c01ff7c08a436acaaeb5fe6d4c0066d
Hadwa Gaber <hadwaa@microsoft.com>
2023-02-17 11:54:55 -0800

    Update Symphony pipeline config cmd (#103)

---
+++ scripts/utilities/os.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+get_current_os(){
+    echo "$OSTYPE"
+}
+
+check_linux_os(){
+    if [[ $OSTYPE == *"linux"* ]]; then
+        return 0
+    fi
+    return 1
+}
+
+check_mac_os(){
+    if [[ $OSTYPE == *"darwin"* ]]; then
+        return 0
+    fi
+    return 1
+}
