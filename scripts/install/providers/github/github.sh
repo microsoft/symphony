67e55a128269704d13ab255d5eb070440057a754
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2023-03-08 15:24:45 -0800

    Changing yes/no label on github provider confirmation

--- scripts/install/providers/github/github.sh
+++ scripts/install/providers/github/github.sh
@@ -30,7 +30,7 @@ function load_inputs {
     fi
 
     if [ -z "$IS_Private_GH_Repo" ]; then
-        _select_yes_no IS_Private_GH_Repo "IS GitHub Repo Private [yes;no]" "false"
+        _select_yes_no IS_Private_GH_Repo "IS GitHub Repo Private (yes/no)" "false"
     fi
 
     if [ -z "$GH_PAT" ]; then
