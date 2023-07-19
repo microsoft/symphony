9dfc6ec0ab9d31bd472a2aad0fc0a31d98ad88db
polatengin <enpolat@microsoft.com>
2023-07-19 00:16:04 +0000

    fixing benchpress tests

--- IAC/Bicep/tests/end_to_end/End_To_End.Tests.ps1
+++ IAC/Bicep/tests/end_to_end/End_To_End.Tests.ps1
@@ -1,49 +1,57 @@
-BeforeDiscovery {
-    . $PSScriptRoot/UtilsLoader.ps1
-}
-
 BeforeAll {
-    Connect-AzAccountFromEnv
+  Import-Module BenchPress.Azure
 
-    $sqlServerResourceGroupName = $env:sqlServerResourceGroupName
-    $sqlServerName = $env:sqlServerName
-    $appServiceResourceGroupName = $env:appServiceResourceGroupName
-    $appServicePlanName = $env:appServicePlanName
-    $appServiceName = $env:appServiceName
+  # arrange
+  $sqlServerResourceGroupName = $env:sqlServerResourceGroupName
+  $sqlServerName = $env:sqlServerName
+  $appServiceResourceGroupName = $env:appServiceResourceGroupName
+  $appServicePlanName = $env:appServicePlanName
+  $appServiceName = $env:appServiceName
 }
 
 Describe "End to End Tests" {
-    Context "Test end to end" {
-        It "should check the parameters and environment variables configured correctly" {
-          $sqlServerResourceGroupName | Should -Not -Be $Null
-          $sqlServerName | Should -Not -Be $Null
-        }
-
-        It "should check the azure powershell connected and configured correctly" {
-          $currentContext = Get-AzContext
-          $currentContext | Should -Not -Be $Null
-        }
-
-        It "resources should be ready and online, and web app works" {
-            $sqlServerResource = Get-SqlServer $sqlServerName $sqlServerResourceGroupName
-            $sqlServerResource.PublicNetworkAccess | Should -Be "Enabled"
-
-            $identitydbResource = Get-SqlDatabase "identitydb" $sqlServerName $sqlServerResourceGroupName
-            $identitydbResource.Status | Should -Be "Online"
-
-            $catalogdbResource = Get-SqlDatabase "catalogdb" $sqlServerName $sqlServerResourceGroupName
-            $catalogdbResource.Status | Should -Be "Online"
-
-            $appServicePlanResource = Get-AppServicePlan $appServicePlanName $appServiceResourceGroupName
-            $appServicePlanResource.Status | Should -Be "Ready"
-            
-            $webAppResource = Get-WebApp $appServiceName $appServiceResourceGroupName
-            $webAppResource.State | Should -Be "Running"
-
-            $defaultHostName = $webAppResource.DefaultHostName
-            $defaultHostName | Should -Not -Be $Null
-
-            $response = Invoke-RestMethod -Uri "http://$defaultHostName" -Method 'Get' -TimeoutSec 240
-        }
+  Context "Test end to end" {
+    It "should check the parameters and environment variables configured correctly" {
+      # act and # assert
+      $sqlServerResourceGroupName | Should -Not -Be $Null
+      $sqlServerName | Should -Not -Be $Null
     }
+
+    It "should check the azure powershell connected and configured correctly" {
+      # act and # assert
+
+      $currentContext = Get-AzContext
+      $currentContext | Should -Not -Be $Null
+    }
+
+    It "resources should be ready and online, and web app works" {
+      # act and # assert
+      $sqlServerResource = Confirm-AzBPSqlServer $sqlServerName $sqlServerResourceGroupName
+      $sqlServerResource.PublicNetworkAccess | Should -Be "Enabled"
+
+      # act and # assert
+      $identitydbResource = Confirm-AzBPSqlDatabase "identitydb" $sqlServerName $sqlServerResourceGroupName
+      $identitydbResource.Status | Should -Be "Online"
+
+      # act and # assert
+      $catalogdbResource = Confirm-AzBPSqlDatabase "catalogdb" $sqlServerName $sqlServerResourceGroupName
+      $catalogdbResource.Status | Should -Be "Online"
+
+      # act and # assert
+      $appServicePlanResource = Confirm-AzBPAppServicePlan $appServicePlanName $appServiceResourceGroupName
+      $appServicePlanResource.Status | Should -Be "Ready"
+
+      # act and # assert
+      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
+      $webAppResource.State | Should -Be "Running"
+
+      # act and # assert
+      $defaultHostName = $webAppResource.DefaultHostName
+      $defaultHostName | Should -Not -Be $null
+
+      # act and # assert
+      $response = Invoke-RestMethod -Uri "http://$defaultHostName" -Method 'Get' -TimeoutSec 240
+      $response | Should -Not -Be $null
+    }
+  }
 }
--- IAC/Bicep/tests/module/02_webapp.Tests.ps1
+++ IAC/Bicep/tests/module/02_webapp.Tests.ps1
@@ -1,67 +1,72 @@
 BeforeAll {
-    . $PSScriptRoot/UtilsLoader.ps1
-    $resourceGroupName = "rgwebapplayer"
-    $appServicePlanName = "test-app-svc-plan-sbg"
-    $appServiceName = "test-app-svc-wj3"
+  Import-Module BenchPress.Azure
+
+  # arrange
+  $resourceGroupName = "rgwebapplayer"
+  $appServicePlanName = "test-app-svc-plan-sbg"
+  $appServiceName = "test-app-svc-wj3"
+
+  # log
+  Write-Host "Running 02_webapp Tests for {resourceGroupName: $resourceGroupName}, {appServicePlanName: $appServicePlanName}, {appServiceName: $appServiceName}"
 }
 
 Describe '02 Web Layer Tests' {
-    it 'Should deploy a resource group for web layer' {
-        #arrange
-        $bicepPath = "../bicep/02_webapp/01_rg/main.bicep"
-        $params = @{
-            deploymentName = "rgwebapplayer"
-            resourceGroupName = $resourceGroupName
-            location = "westus"
-            environment = "test"
-        }
-
-        #act
-        $deployment = Deploy-BicepFeature $bicepPath $params
-        $resourceGroupExists = Get-ResourceGroupExists $resourceGroupName
-
-        #assert
-        $deployment.ProvisioningState | Should -Be "Succeeded"
-        $resourceGroupExists | Should -Be $true
+  it 'Should deploy a resource group for web layer' {
+    #arrange
+    $bicepPath = "../bicep/02_webapp/01_rg/main.bicep"
+    $params = @{
+      deploymentName    = "rgwebapplayer"
+      resourceGroupName = $resourceGroupName
+      location          = "westus"
+      environment       = "test"
     }
 
-    it 'Should deploy an app service' {
-        #arrange
-        $bicepPath = "../bicep/02_webapp/02_deployment/main.bicep"
-        $params = @{
-            deploymentName = "rgwebapplayer"
-            location = "westus"
-            environment = "test"
-            appSvcPlanSkuName = "S1"
-            appSvcPlanSkuTier = "Standard"
-            appSvcDockerImage = "crsymphony360.azurecr.io/eshoppublicapi"
-            appSvcDockerImageTag = "a87f571"
-            containerRegistryResourceGroupName = "devops-symphony-362"
-            containerRegistryName = "crsymphony360"
-            sqlDatabaseCatalogDbName = "sqlDatabaseCatalogDbName"
-            sqlDatabaseIdentityDbName = "sqlDatabaseIdentityDbName"
-            sqlServerFqdn = "sqlServerFqdn"
-            sqlServerAdministratorLogin = "sqlServerAdministratorLogin"
-            sqlServerAdministratorPassword = "sqlServerAdministratorPassword"
-        }
+    #act
+    $deployment = Deploy-AzBPBicepFeature $bicepPath $params
+    $resourceGroupExists = Get-ResourceGroupExists $resourceGroupName
 
-        #act
-        $deployment = Deploy-BicepFeature $bicepPath $params $resourceGroupName
-        $appServicePlanResourceExists = Get-AppServicePlanExists $appServicePlanName $resourceGroupName
-        $webAppResourceExists = Get-WebAppExists $appServiceName $resourceGroupName
+    #assert
+    $deployment.ProvisioningState | Should -Be "Succeeded"
+    $resourceGroupExists | Should -Be $true
+  }
 
-        #assert
-        $deployment.ProvisioningState | Should -Be "Succeeded"
-        $appServicePlanResourceExists | Should -Be $true
-        $webAppResourceExists | Should -Be $true
+  it 'Should deploy an app service' {
+    #arrange
+    $bicepPath = "../bicep/02_webapp/02_deployment/main.bicep"
+    $params = @{
+      deploymentName                     = "rgwebapplayer"
+      location                           = "westus"
+      environment                        = "test"
+      appSvcPlanSkuName                  = "S1"
+      appSvcPlanSkuTier                  = "Standard"
+      appSvcDockerImage                  = "crsymphony360.azurecr.io/eshoppublicapi"
+      appSvcDockerImageTag               = "a87f571"
+      containerRegistryResourceGroupName = "devops-symphony-362"
+      containerRegistryName              = "crsymphony360"
+      sqlDatabaseCatalogDbName           = "sqlDatabaseCatalogDbName"
+      sqlDatabaseIdentityDbName          = "sqlDatabaseIdentityDbName"
+      sqlServerFqdn                      = "sqlServerFqdn"
+      sqlServerAdministratorLogin        = "sqlServerAdministratorLogin"
+      sqlServerAdministratorPassword     = "sqlServerAdministratorPassword"
     }
+
+    #act
+    $deployment = Deploy-AzBPBicepFeature $bicepPath $params $resourceGroupName
+    $appServicePlanResourceExists = Get-AppServicePlanExists $appServicePlanName $resourceGroupName
+    $webAppResourceExists = Get-WebAppExists $appServiceName $resourceGroupName
+
+    #assert
+    $deployment.ProvisioningState | Should -Be "Succeeded"
+    $appServicePlanResourceExists | Should -Be $true
+    $webAppResourceExists | Should -Be $true
+  }
 }
 
 AfterAll {
-    #clean up
-    Write-Host "Cleaning up Resources!"
+  #clean up
+  Write-Host "Cleaning up Resources!"
 
-    Write-Host "Removing Resource Group $resourceGroupName"
+  Write-Host "Removing Resource Group $resourceGroupName"
 
-    Remove-BicepFeature $resourceGroupName
+  Remove-AzBPBicepFeature $resourceGroupName
 }
