d0543081c0ce30b92a5a529e72dec9f2e3dde643
polatengin <enpolat@microsoft.com>
2023-07-26 00:16:12 +0000

    formatting document

--- IAC/Bicep/tests/end_to_end/End_To_End.Tests.ps1
+++ IAC/Bicep/tests/end_to_end/End_To_End.Tests.ps1
@@ -12,44 +12,44 @@ BeforeAll {
 Describe "End to End Tests" {
   Context "Test end to end" {
     It "should check the parameters and environment variables configured correctly" {
-      # act and # assert
+      # act and assert
       $sqlServerResourceGroupName | Should -Not -Be $Null
       $sqlServerName | Should -Not -Be $Null
     }
 
     It "should check the azure powershell connected and configured correctly" {
-      # act and # assert
+      # act and assert
 
       $currentContext = Get-AzContext
       $currentContext | Should -Not -Be $Null
     }
 
     It "resources should be ready and online, and web app works" {
-      # act and # assert
+      # act and assert
       $sqlServerResource = Confirm-AzBPSqlServer $sqlServerName $sqlServerResourceGroupName
       $sqlServerResource.PublicNetworkAccess | Should -Be "Enabled"
 
-      # act and # assert
+      # act and assert
       $identitydbResource = Confirm-AzBPSqlDatabase "identitydb" $sqlServerName $sqlServerResourceGroupName
       $identitydbResource.Status | Should -Be "Online"
 
-      # act and # assert
+      # act and assert
       $catalogdbResource = Confirm-AzBPSqlDatabase "catalogdb" $sqlServerName $sqlServerResourceGroupName
       $catalogdbResource.Status | Should -Be "Online"
 
-      # act and # assert
+      # act and assert
       $appServicePlanResource = Confirm-AzBPAppServicePlan $appServicePlanName $appServiceResourceGroupName
       $appServicePlanResource.Status | Should -Be "Ready"
 
-      # act and # assert
+      # act and assert
       $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
       $webAppResource.State | Should -Be "Running"
 
-      # act and # assert
+      # act and assert
       $defaultHostName = $webAppResource.DefaultHostName
       $defaultHostName | Should -Not -Be $null
 
-      # act and # assert
+      # act and assert
       $response = Invoke-RestMethod -Uri "http://$defaultHostName" -Method 'Get' -TimeoutSec 240
       $response | Should -Not -Be $null
     }
