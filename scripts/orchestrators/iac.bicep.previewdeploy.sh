fa5b96772984ffa71aad146b2035c2f357e9a755
Hadwa Gaber <hadwaa@microsoft.com>
2023-09-20 15:57:43 -0700

    update deploy to use bicepparam files

--- scripts/orchestrators/iac.bicep.previewdeploy.sh
+++ scripts/orchestrators/iac.bicep.previewdeploy.sh
@@ -41,10 +41,10 @@ for deployment in "${modules[@]}"; do
     params=()
     SAVEIFS=${IFS}
     IFS=$'\n'
-    params=($(find "${WORKSPACE_PATH}/env/bicep/${ENVIRONMENT_NAME}" -maxdepth 1 -type f -name '*parameters*.json'))
+    #params=($(find "${WORKSPACE_PATH}/env/bicep/${ENVIRONMENT_NAME}" -maxdepth 1 -type f -name '*parameters*.json'))
     param_tmp_deployment="${WORKSPACE_PATH}/env/bicep/${ENVIRONMENT_NAME}/${path//.\//}/"
     if [[ -d "${param_tmp_deployment}" ]]; then
-        params+=($(find "${param_tmp_deployment}" -maxdepth 1 -type f -name '*parameters*.json' -and -not -name '*mockup*'))
+        params+=($(find "${param_tmp_deployment}" -maxdepth 1 -type f -name '*parameters*.bicepparam'))
     fi
     IFS=${SAVEIFS}
 
