ffa69083604059791e086be5729dae8db90e241f
Hadwa Gaber <hadwaa@microsoft.com>
2023-09-21 13:26:49 -0700

    update terraform events to add pre_validate event

--- scripts/orchestrators/iac.tf.validate.sh
+++ scripts/orchestrators/iac.tf.validate.sh
@@ -18,6 +18,16 @@ for deployment in "${array[@]}"; do
 
         init true "${ENVIRONMENT_NAME}${deployment}.tfstate" "${ARM_SUBSCRIPTION_ID}" "${ARM_TENANT_ID}" "${ARM_CLIENT_ID}" "${ARM_CLIENT_SECRET}" "${STATE_STORAGE_ACCOUNT}" "${STATE_CONTAINER}" "${STATE_RG}"
         echo "tf init ${deployment}"
+
+        layer_folder_path=$(dirname "${deployment}")
+        if [ -f "${layer_folder_path}/_events.sh" ]; then
+        source "${layer_folder_path}/_events.sh"
+        fi
+
+        if [ "$(type -t pre_validate)" == "function" ]; then
+            pre_validate
+        fi
+
         echo "tf validate ${deployment}"
         validate
         code=$?
@@ -25,6 +35,12 @@ for deployment in "${array[@]}"; do
             echo "terraform validate - returned code ${code}"
             exit $code
         fi
+        if [ "$(type -t post_validate)" == "function" ]; then
+            post_validate
+        fi
+        unset -f pre_validate
+        unset -f post_validate
+
         popd
 
     fi
