4452988d02f1a796be1572fb6c55e1c4e0b42497
Hadwa Gaber <hadwaa@microsoft.com>
2023-09-26 09:53:11 -0700

    Move to bicepparam (#224)
    * replace bicep env json files with .bicepparam files

--- IAC/Bicep/bicep/02_webapp/02_deployment/_events.sh
+++ IAC/Bicep/bicep/02_webapp/02_deployment/_events.sh
@@ -11,3 +11,20 @@ post_deploy() {
   # placeholder for post deploy logic
   return $?
 }
+
+# pre_validate method is called before the deployment validation
+pre_validate() {
+  # placeholder for pre deployment validate logic
+  export sqlDatabaseCatalogDbName="sqlDatabaseCatalogDbName"
+  export sqlDatabaseIdentityDbName="sqlDatabaseIdentityDbName"
+  export sqlServerFqdn="sqlServerFqdn"
+  export sqlServerAdministratorLogin="eshop"
+  export sqlServerAdministratorPassword="eshO@#12csdf"
+  return $?
+}
+
+# post_validate method is called after the deployment validation
+post_validate() {
+  # placeholder for post deployment validate logic
+  return $?
+}
---
+++ env/bicep/prod/01_sql/01_rg/parameters.bicepparam
@@ -0,0 +1,5 @@
+using './../../../../../IAC/Bicep/bicep/01_sql/01_rg/main.bicep'
+
+param environment= 'prod'
+param location=readEnvironmentVariable('LOCATION_NAME','westus')
+param layerName=readEnvironmentVariable('layerName')
