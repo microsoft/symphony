7d9f90baaa36323af83e718ffc598817cc3da3da
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2023-10-03 15:58:17 -0400

    Added KEY_VAULT_NAME env var

--- scripts/orchestrators/github.loadsecrets.sh
+++ scripts/orchestrators/github.loadsecrets.sh
@@ -4,7 +4,7 @@ OIFS=$IFS
 IFS=', '
 for secretname in $SECRETS
 do
-    secret=$(az keyvault secret show --name $secretname --vault-name ${{ inputs.keyVaultName }} --query "value")
+    secret=$(az keyvault secret show --name $secretname --vault-name $KEY_VAULT_NAME --query "value")
     echo "::add-mask::$secret"
     echo "$secretname=$secret" >> $GITHUB_OUTPUT
 done
