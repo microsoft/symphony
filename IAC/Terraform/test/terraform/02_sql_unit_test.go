bdeaccb23b9ab8617a09f3049dd8d4e12d6bad69
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-11-06 12:06:39 -0800

    go linter fixes

--- IAC/Terraform/test/terraform/02_sql_unit_test.go
+++ IAC/Terraform/test/terraform/02_sql_unit_test.go
@@ -14,51 +14,51 @@ import (
 )
 
 func Test02_SQL(t *testing.T) {
-	t.Parallel()
+  t.Parallel()
 
-	location := "westus"
-	env := "dev"
-	//load remote state env vars
-	rmResourceGroupName := os.Getenv("resource_group_name")
-	rmStorageAccName := os.Getenv("storage_account_name")
-	rmContainerName := os.Getenv("container_name")
-	rmKey := "02_sql/01_deployment_test.tfstate"
+  location := "westus"
+  env := "dev"
+  //load remote state env vars
+  rmResourceGroupName := os.Getenv("resource_group_name")
+  rmStorageAccName := os.Getenv("storage_account_name")
+  rmContainerName := os.Getenv("container_name")
+  rmKey := "02_sql/01_deployment_test.tfstate"
 
-	// Configure Terraform setting up a path to Terraform code.
-	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: "../../terraform/02_sql/01_deployment",
+  // Configure Terraform setting up a path to Terraform code.
+  terraformOptions := &terraform.Options{
+    // The path to where our Terraform code is located
+    TerraformDir: "../../terraform/02_sql/01_deployment",
 
-		// Variables to pass to init remote state
-		BackendConfig: map[string]interface{}{
-			"resource_group_name":  rmResourceGroupName,
-			"container_name":       rmContainerName,
-			"storage_account_name": rmStorageAccName,
-			"key":                  rmKey},
+    // Variables to pass to init remote state
+    BackendConfig: map[string]interface{}{
+      "resource_group_name":  rmResourceGroupName,
+      "container_name":       rmContainerName,
+      "storage_account_name": rmStorageAccName,
+      "key":                  rmKey},
 
-		Vars: map[string]interface{}{
-			"location": location,
-			"env":      env,
-		},
-		Reconfigure: true,
-	}
+    Vars: map[string]interface{}{
+      "location": location,
+      "env":      env,
+    },
+    Reconfigure: true,
+  }
 
-	// Defer 'terraform Destroy'
-	defer terraform.Destroy(t, terraformOptions)
+  // Defer 'terraform Destroy'
+  defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` to init remote state.
-	terraform.InitAndApply(t, terraformOptions)
+  // Run `terraform init` to init remote state.
+  terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the values of output variables from the terraform.tfstate
-	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
-	sqlServerName := terraform.Output(t, terraformOptions, "sql_server_name")
+  // Run `terraform output` to get the values of output variables from the terraform.tfstate
+  resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+  sqlServerName := terraform.Output(t, terraformOptions, "sql_server_name")
 
-	catalogDBName := terraform.Output(t, terraformOptions, "catalog_sql_db_name")
-	identityDBName := terraform.Output(t, terraformOptions, "identity_sql_db_name")
-	expectedSQLDBStatus := "Online"
+  catalogDBName := terraform.Output(t, terraformOptions, "catalog_sql_db_name")
+  identityDBName := terraform.Output(t, terraformOptions, "identity_sql_db_name")
+  expectedSQLDBStatus := "Online"
 
-	// Assert deployed server and databases status
-	assert.Equal(t, sql.ServerStateReady, azure.GetSQLServer(t, resourceGroupName, sqlServerName, "").State, "SQl server Status")
-	assert.Equal(t, expectedSQLDBStatus, *azure.GetSQLDatabase(t, resourceGroupName, sqlServerName, catalogDBName, "").Status, "Catalog SQL DB Status")
-	assert.Equal(t, expectedSQLDBStatus, *azure.GetSQLDatabase(t, resourceGroupName, sqlServerName, identityDBName, "").Status, "Identity SQL DB Status")
+  // Assert deployed server and databases status
+  assert.Equal(t, sql.ServerStateReady, azure.GetSQLServer(t, resourceGroupName, sqlServerName, "").State, "SQl server Status")
+  assert.Equal(t, expectedSQLDBStatus, *azure.GetSQLDatabase(t, resourceGroupName, sqlServerName, catalogDBName, "").Status, "Catalog SQL DB Status")
+  assert.Equal(t, expectedSQLDBStatus, *azure.GetSQLDatabase(t, resourceGroupName, sqlServerName, identityDBName, "").Status, "Identity SQL DB Status")
 }
