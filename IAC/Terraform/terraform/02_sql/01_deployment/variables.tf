fcdb02f2fb34bfddca34c5c10a5c8c31a722568d
Hadwa Gaber <hadwaa@microsoft.com>
2024-01-17 15:02:57 -0800

    Add github pull request workflow for terraform (#246)

--- IAC/Terraform/terraform/02_sql/01_deployment/variables.tf
+++ IAC/Terraform/terraform/02_sql/01_deployment/variables.tf
@@ -1,9 +1,9 @@
 variable "location" {
   description = "The supported azure location where the resource deployed"
-  type = string
+  type        = string
 }
 
 variable "env" {
   description = "The name of the environment to be deployed"
-  type = string
+  type        = string
 }
