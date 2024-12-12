94ec532fb1beb6dc93531aabc17d1a42e9cffbba
polatengin <enpolat@microsoft.com>
2024-12-12 22:29:25 +0000

    updating end to end tests

--- IAC/Bicep/test/end_to_end/End_To_End.Tests.ps1
+++ IAC/Bicep/test/end_to_end/End_To_End.Tests.ps1
@@ -2,87 +2,53 @@ BeforeAll {
   Import-Module BenchPress.Azure
 
   # arrange
-  $sqlServerResourceGroupName = $env:sqlServerResourceGroupName
-  $sqlServerName = $env:sqlServerName
-  $appServiceResourceGroupName = $env:appServiceResourceGroupName
-  $appServicePlanName = $env:appServicePlanName
-  $appServiceName = $env:appServiceName
+  $storageAccountResourceGroupName = $env:storageAccountResourceGroupName
+  $storageAccountName = $env:storageAccountName
+  $appConfigResourceGroupName = $env:appConfigResourceGroupName
+  $appConfigAccountName = $env:appConfigAccountName
+  $appConfigItemsLength = $env:appConfigItemsLength
 }
 
 Describe "End to End Tests" {
   Context "Test end to end" {
     It "should check the parameters and environment variables configured correctly" {
       # act and assert
-      $sqlServerResourceGroupName | Should -Not -Be $Null
-      $sqlServerName | Should -Not -Be $Null
+      $storageAccountResourceGroupName | Should -Not -Be $Null
+      $storageAccountName | Should -Not -Be $Null
+      $appConfigResourceGroupName | Should -Not -Be $Null
+      $appConfigAccountName | Should -Not -Be $Null
+      $appConfigItemsLength | Should -BeGreaterThan 0
     }
 
-    It "should check the sql server is ready" {
+    It "should check the storage account is ready" {
       # act and assert
-      $sqlServerResource = Confirm-AzBPSqlServer $sqlServerName $sqlServerResourceGroupName
-      $sqlServerResource.ResourceDetails.PublicNetworkAccess | Should -Be "Enabled"
+      $storageAccountResource = Confirm-AzBPStorageAccount -ResourceGroupName $storageAccountResourceGroupName -Name $storageAccountName
+      $storageAccountResource | Should -Be -Not $null
     }
 
-    It "should check the identity database is online and ready" {
+    It "should check the storage account is configured correctly" {
       # act and assert
-      $identitydbResource = Confirm-AzBPSqlDatabase "identitydb" $sqlServerName $sqlServerResourceGroupName
-      $identitydbResource.ResourceDetails.Status | Should -Be "Online"
+      $storageAccountResource = Confirm-AzBPStorageAccount -ResourceGroupName $storageAccountResourceGroupName -Name $storageAccountName
+
+      $storageAccountResource.ResourceDetails.ProvisioningState | Should -Be "Succeeded"
+      $storageAccountResource.ResourceDetails.Kind | Should -Be "StorageV2"
+      $storageAccountResource.ResourceDetails.Sku.Name | Should -Be "Standard_LRS"
+      $storageAccountResource.ResourceDetails.EnableHttpsTrafficOnly | Should -Be $true
+      $storageAccountResource.ResourceDetails.MinimumTlsVersion | Should -Be "TLS1_2"
     }
 
-    It "should check the catalog database is online and ready" {
+    It "should check the app config is ready" {
       # act and assert
-      $catalogdbResource = Confirm-AzBPSqlDatabase "catalogdb" $sqlServerName $sqlServerResourceGroupName
-      $catalogdbResource.ResourceDetails.Status | Should -Be "Online"
+      $appConfigResource = Get-AzBPResource -ResourceName $appConfigAccountName -ResourceGroupName $appConfigResourceGroupName
+
+      $appConfigResource | Should -Be -Not $null
     }
 
-    It "should check the app service plan is ready" {
+    It "should check the app config is running" {
       # act and assert
-      $appServicePlanResource = Confirm-AzBPAppServicePlan $appServicePlanName $appServiceResourceGroupName
-      $appServicePlanResource.ResourceDetails.Status | Should -Be "Ready"
-    }
+      $appConfigResource = Get-AzBPResource -ResourceName $appConfigAccountName -ResourceGroupName $appConfigResourceGroupName
 
-    It "should check the web app is running" {
-      # act and assert
-      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
-      $webAppResource.ResourceDetails.State | Should -Be "Running"
-    }
-
-    It "should check the web app has connection strings" {
-      # act and assert
-      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
-      $webAppResource.ResourceDetails.SiteConfig.ConnectionStrings | Should -Not -Be $null
-    }
-
-    It "should check the web app has two connection strings" {
-      # act and assert
-      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
-      $webAppResource.ResourceDetails.SiteConfig.ConnectionStrings.Count | Should -Be 2
-    }
-
-    It "should check the web app has the catalog connection string with the correct sql server name" {
-      # act and assert
-      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
-      $webAppResource.ResourceDetails.SiteConfig.ConnectionStrings | Where-Object { $_.name -eq 'CatalogConnection' } | Select-Object -ExpandProperty ConnectionString | Should -BeLike "*$sqlServerName*"
-    }
-
-    It "should check the web app has the identity connection string with the correct sql server name" {
-      # act and assert
-      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
-      $webAppResource.ResourceDetails.SiteConfig.ConnectionStrings | Where-Object { $_.name -eq 'IdentityConnection' } | Select-Object -ExpandProperty ConnectionString | Should -BeLike "*$sqlServerName*"
-    }
-
-    It "should check the web app has a host name" {
-      # act and assert
-      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
-      $webAppResource.ResourceDetails.DefaultHostName | Should -Not -Be $null
-    }
-
-    It "should check the web app works" {
-      # act and assert
-      $webAppResource = Confirm-AzBPWebApp $appServiceName $appServiceResourceGroupName
-      $defaultHostName = $webAppResource.ResourceDetails.DefaultHostName
-      $response = Invoke-RestMethod -Uri "http://$defaultHostName" -Method 'Get' -TimeoutSec 240
-      $response | Should -Not -Be $null
+      $appConfigResource[1].Success | Should -Be $true
     }
   }
 }
