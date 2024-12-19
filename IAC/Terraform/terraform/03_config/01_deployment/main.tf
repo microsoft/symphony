85d5d8059c2066c918755a859160b0a3294c3cd2
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2024-12-19 12:21:41 -0500

    tf sample fixes

--- IAC/Terraform/terraform/03_config/01_deployment/main.tf
+++ IAC/Terraform/terraform/03_config/01_deployment/main.tf
@@ -41,10 +41,7 @@ resource "azurerm_app_configuration" "appconfig" {
   name                = azurecaf_name.app_configuration.result
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  sku = {
-    name = "Standard"
-  }
+  sku                 = "standard"
 }
 
 # ------------------------------------------------------------------------------------------------------
